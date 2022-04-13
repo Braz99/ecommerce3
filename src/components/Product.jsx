af63bfd17415b2bbb2caa9e2bc1f5a9ae904f22f
braz99 <fabriciobrazil99@gmail.com>
2022-04-12 21:42:20 -0300

    Muda a exibição dos produtos na página home

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,55 +6,56 @@ import addToCart from "../store/actions/addToCart";
 import updateProduct from "../store/actions/updateProduct";
 
 export default function Product({ product }) {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const { cart } = useSelector((state) => state);
+  const { cart } = useSelector((state) => state);
 
-	let model = {
-		id: 0,
-		name: "",
-		price: 0,
-		image: "",
-		quantity: 1,
-	};
+  let model = {
+    id: 0,
+    name: "",
+    price: 0,
+    image: "",
+    quantity: 1,
+  };
 
-	function handleAddProduct(product, e) {
-		e.preventDefault();
-		model.id = product.id;
-		model.name = product.name;
-		model.price = product.price;
-		model.image = product.image;
+  function handleAddProduct(product, e) {
+    e.preventDefault();
+    model.id = product.id;
+    model.name = product.name;
+    model.price = product.price;
+    model.image = product.image;
 
-		const find = cart.find((item) => item.id === model.id);
+    const find = cart.find((item) => item.id === model.id);
 
-		if (find) {
-			dispatch(updateProduct(model.id));
-		} else {
-			dispatch(addToCart(model));
-		}
+    if (find) {
+      dispatch(updateProduct(model.id));
+    } else {
+      dispatch(addToCart(model));
+    }
 
-		toast.success("Produto adicionado ao carrinho!");
-	}
+    toast.success("Produto adicionado ao carrinho!");
+  }
 
-	return (
-		<li className="product" id={product.id}>
-			<h3 className="product-name">{product.name}</h3>
+  return (
+    <li className="product" id={product.id}>
+      <img
+        className="product-image"
+        src={`assets/${product.image}`}
+        alt={product.image}
+      />
 
-			<img
-				className="product-image"
-				src={`assets/${product.image}`}
-				alt={product.image}
-			/>
+      <h3 className="product-name">{product.name}</h3>
 
-			<h3 className="product-price">
-				R$ {product.price.toFixed(2).replace(".", ",")}
-			</h3>
+      <h3 className="product-price">
+        R$ {product.price.toFixed(2).replace(".", ",")}
+      </h3>
 
-			<button
-				className="product-button"
-				onClick={(e) => handleAddProduct(product, e)}>
-				Adicionar ao carrinho
-			</button>
-		</li>
-	);
+      <button
+        className="product-button"
+        onClick={(e) => handleAddProduct(product, e)}
+      >
+        Adicionar ao carrinho
+      </button>
+    </li>
+  );
 }
